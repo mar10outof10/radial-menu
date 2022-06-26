fed996f1aa589198ff13ec6939f049cf820da8b0
Martin Halas <marhaldev@gmail.com>
2022-06-26 13:41:46 -0400

    Installed sass, more skeleton setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Counter } from 'features/counter/Counter';
-import 'App.css';
+import 'App.scss';
 
 function App() {
   return (
---
+++ src/RadialMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from 'app/store';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
+import 'RadialMenu.scss';
+
+const RadialMenu = () => {
+
+};
+
+export default RadialMenu;
